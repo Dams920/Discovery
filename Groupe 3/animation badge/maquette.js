3e0f9faa29bc047ff7bea8c81ddc473de050c0f0
LANNEY Remi <r.lanney@ics-nice.com>
2021-01-14 15:20:42 +0100

    scroll

--- Groupe 3/animation badge/maquette.js
+++ Groupe 3/animation badge/maquette.js
@@ -2,30 +2,19 @@
 function cerclenoir(){
     /*cercle noir*/
     var element = document.getElementById('cerclenoir');
-element.parentNode.removeChild(element);
+    element.parentNode.removeChild(element);
 
-/*Alan*/
-var element2=document.getElementById('alan');
-element2.setAttribute("src", "alan_explain.png");
+    /*Alan*/
+    var element2=document.getElementById('alan');
+    // alert(element2.style.backgroundImage);
+    element2.setAttribute("style", 'background-image: url("alan_explain.png");');
+    //element2.setAttribute("src", "alan_explain.png");
 
-/*text*/
-document.getElementById("recup").innerHTML = "Bien </br> joué!";
+    /*text*/
+    document.getElementById("recup").innerHTML = "Bien </br> joué!";
 
-
-/*img.attributes('id', 'alan');*/
-
-
-
-
-/*$().css({
-    width: 50%;
-       position: absolute;
-       top: 13px;
-       left: 0;
-       right: 3em;
-});*/
-
-
+    $("#invisible").css("display", "block");
+    goTo("#invisible",1500);    
 }
 
 function resize(){
@@ -37,3 +26,13 @@ window.addEventListener("resize", resize);
 window.addEventListener("load", resize); 
 
 
+function goTo(selector, timeout, cb) {
+    // alert(timeout);
+    var $el = $(selector);
+    if (!$el[0]) return;
+    var $par = $el.parent();
+    if ($par.is("body")) $par = $("html, body");
+    setTimeout(() => {
+      $par.stop().animate({scrollTop: $el.offset().top}, 1000, cb && cb.call($el[0]));
+    }, timeout || 0);
+  }
