27da10e83d920b1dae899525857ffe9fa5ad03e5
LANNEY Remi <r.lanney@ics-nice.com>
2021-01-14 15:36:35 +0100

    store

--- Groupe 3/animation badge/maquette.js
+++ Groupe 3/animation badge/maquette.js
@@ -23,7 +23,13 @@ function resize(){
 }
 
 window.addEventListener("resize", resize);
-window.addEventListener("load", resize); 
+window.addEventListener("load", function(){
+    resize();
+    document.getElementById("cercle").addEventListener("click", function(){
+        redirectionStore();
+    }); 
+}); 
+
 
 
 function goTo(selector, timeout, cb) {
@@ -35,4 +41,18 @@ function goTo(selector, timeout, cb) {
     setTimeout(() => {
       $par.stop().animate({scrollTop: $el.offset().top}, 1000, cb && cb.call($el[0]));
     }, timeout || 0);
+  }
+
+  function redirectionStore() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;  
+      if (/android/i.test(userAgent)) {
+          return document.getElementById("demo").href = "https://play.google.com/apps?gl=FR";
+      }
+      // iOS detection from: http://stackoverflow.com/a/9039885/177710
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+          return document.getElementById("demo").href = "https://www.apple.com/fr/ios/app-store/";
+      }
+     else{
+        return 0;
+     }
   }
